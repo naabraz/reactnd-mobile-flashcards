480de0b36dfff08a5f83fd0e201bea3b532aaee8
Natalia Braz <naabraz@gmail.com>
2018-11-02 21:12:38 -0300

    Adjusting main navigator

--- components/MainNavigator.js
+++ components/MainNavigator.js
@@ -7,37 +7,29 @@ import { steelTeal, white } from './style/utils/colors'
 import Decks from './Decks'
 import NewDeck from './NewDeck'
 
-const Tabs = createBottomTabNavigator({
-  Decks: {
-    screen: Decks,
-    navigationOptions: {
-      tabBarLabel: 'Decks',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    }
+export default MainNavigator = createBottomTabNavigator({
+    Decks: {
+      screen: Decks, 
+      navigationOptions: { 
+        tabBarLabel: 'My Decks',
+        tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} /> 
+      }
+    },
+    NewDeck: { 
+      screen: NewDeck,
+      navigationOptions: { 
+        tabBarLabel: 'Add Deck',
+        tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} /> 
+      }
+    },
   },
-  NewDeck: {
-    screen: NewDeck,
-    navigationOptions: {
-      tabBarLabel: 'New Deck',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    }
-  },
-},
-{
-  tabBarOptions: {
-    activeTintColor: white,
-    style: {
-      height: 56,
-      backgroundColor: steelTeal,
+  {
+    tabBarOptions: {
+      activeTintColor: white,
+      style: {
+        height: 60,
+        backgroundColor: steelTeal,
+      }
     }
   }
-})
-
-export default MainNavigator = createStackNavigator({
-  Home: {
-    screen: Tabs,
-  },
-  Decks: {
-    screen: Decks,
-  }
-})
+)
