21a297123e94c396d019663937b0ad6ccce525cb
Natalia Braz <naabraz@gmail.com>
2018-11-05 13:02:58 -0200

    Adding button to get Deck Info

--- components/Decks.js
+++ components/Decks.js
@@ -6,15 +6,18 @@ import { Title } from './style/Title'
 import { DeckList } from './style/List'
 import { DeckName } from './style/Text'
 import { Wrapper } from './style/Wrapper'
+import { DeckButton } from './style/Button'
 
 class Decks extends Component {
+  getDeck = () => console.log('getDeck')
+
   render() {
     return (
       <Wrapper>
         <Title>Decks</Title>
           <DeckList
             data={Object.keys(mockDecks)}
-            renderItem={({ item }) => <DeckName>{mockDecks[item].title}</DeckName>}
+            renderItem={({ item }) => <DeckButton onPress={this.getDeck}><DeckName>{mockDecks[item].title}</DeckName></DeckButton>}
             keyExtractor={(index) => index.toString()}
           />
       </Wrapper>
